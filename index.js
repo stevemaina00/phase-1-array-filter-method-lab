865323bd0f063f102f5cd7f4d19ee33caa81becd
Steve <steve.maina@student.moringaschool.com>
2022-08-22 10:48:54 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'], hometown=['Pittsburgh', 'New York', 'Cleveland', 'Los Angeles', 'Tampa Bay']) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === hometown.toLowerCase() })
+  }
+ 
+  function fuzzyMatch(drivers= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'], hometown=['Pittsburgh', 'New York', 'Cleveland', 'Los Angeles', 'Tampa Bay']) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, hometown.length) === hometown.toLowerCase() })
+  }
+ 
+  function matchName(drivers= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'], match) {
+    return drivers.filter(function (driver) { return driver.name === match })
+  }
